d28c65ed935d14b62f1d3b98c4c6b51274b1b617
endi julian <endi@iglobal.co.id>
2021-04-09 14:18:44 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<head>
+
+    @include('includes.meta')
+
+    <title>@yield('title') | Landing Page</title>
+
+    <link rel="apple-touch-icon" href="">
+    <link rel="shortcut icon" type="image/x-icon" href="">
+
+    @stack('before-style')
+        @include('includes.style')
+    @stack('after-style')
+</head>
+<body>
+
+    @include('includes.header')
+        @yield('content')
+    @include('includes.footer')
+    
+
+    @stack('before-script')
+        @include('includes.script')
+    @stack('after-scirpt')
+</body>
+</html>
